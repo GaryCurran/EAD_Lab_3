f7774acfa3dc991af093d25567f45456feb50b97
GaryCurran <garycurran@live.com>
2014-09-29 15:19:36 +0100

    New variable

---
+++ EAD_Lab_3/EAD_Lab_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAD_Lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String test = "Hello";
+            Console.WriteLine(test);
+            Console.ReadLine();
+
+        }
+    }
+}
